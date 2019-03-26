678f416d4ac95668ee0582ed2c12c433ce4a31e5
grzegorzogar <48452481+grzegorzogar@users.noreply.github.com>
2019-03-26 18:40:58 +0100

    Add files via upload

---
+++ dodawanie liczb w ciągu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int suma=0;
+    cin >> i;
+
+        for(int j=1; i>=j ; j++)
+        suma +=j;
+
+    cout << "Suma wynosi: " <<suma<<endl; ;
+    return 0;
+}
